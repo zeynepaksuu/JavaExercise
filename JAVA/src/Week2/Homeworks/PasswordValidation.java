03330c0db26f32d2f3b0d2454db5a990c3e404ea
Zeynep Aksu <zeynepgspor@gmail.com>
2025-07-27 23:42:22 +0300

    validation

--- JAVA/src/Week2/Homeworks/PasswordValidation.java
+++ JAVA/src/Week2/Homeworks/PasswordValidation.java
@@ -1,4 +1,36 @@
 package Week2.Homeworks;
+import java.util.Scanner;
 
 public class PasswordValidation {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("PLease enter a password:");
+        String password = scanner.nextLine();
+
+        boolean valid = true;
+        if (password.length() < 8) {
+            valid = false;
+        }
+
+        for (int i = 0; i < password.length(); i++) {
+            if ( password.charAt(i) == ' ') {
+                valid = false;
+                break;
+            }
+        }
+        char firstChar = password.charAt(0);
+        if (!(firstChar >= 'A' && firstChar <= 'Z')) {
+            valid = false;
+        }
+        if (password.charAt(password.length() - 1) != '?') {
+            valid = false;
+        }
+
+        if (valid) {
+            System.out.println("Valid password.");
+        } else {
+            System.out.println("Invalid password");
+        }
+    }
 }
