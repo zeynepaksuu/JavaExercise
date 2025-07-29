46b4fee7d432459092a3a542a0051701baf8908c
Zeynep Aksu <zeynepgspor@gmail.com>
2025-07-29 17:56:07 +0300

    added

--- JAVA/src/Week2/Exercises/Practices/PowerNumberExample.java
+++ JAVA/src/Week2/Exercises/Practices/PowerNumberExample.java
@@ -1,4 +1,22 @@
 package Week2.Exercises.Practices;
+import java.util.Scanner;
 
 public class PowerNumberExample {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Taban değerini giriniz: ");
+        int base = scanner.nextInt();
+        System.out.println("Üs değerini giriniz: ");
+        int power = scanner.nextInt();
+
+        int result = power( base, power);
+        System.out.println(base + "^" + power + "=" + result);
+    }
+
+    private static int power (int base , int power) {
+        if ( power == 0)  return 1;
+        return base * power(base, power-1 );
+
+    }
 }
