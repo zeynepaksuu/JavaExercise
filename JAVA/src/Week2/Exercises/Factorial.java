eb02dbe688114ba7b918d16ebe54372f55514303
Zeynep Aksu <zeynepgspor@gmail.com>
2025-07-29 17:05:20 +0300

    added

--- JAVA/src/Week2/Exercises/Factorial.java
+++ JAVA/src/Week2/Exercises/Factorial.java
@@ -1,4 +1,16 @@
 package Week2.Exercises;
 
 public class Factorial {
+
+    public static void main(String[] args) {
+        int result = factorial(5); // 5! = 120
+        System.out.println(result);
+    }
+
+    public static int factorial(int number) {
+        if (number == 0) {
+            return 1;
+        }
+        return number * factorial(number - 1);
+    }
 }
