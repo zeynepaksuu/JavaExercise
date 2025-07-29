a523363898e034b3c876f96c1835c9b9b1cc96d4
Zeynep Aksu <zeynepgspor@gmail.com>
2025-07-29 17:04:30 +0300

    added

---
+++ JAVA/src/Week2/Exercises/Calculation.java
@@ -0,0 +1,18 @@
+package Week2.Exercises;
+
+public class Calculation {
+
+    double PI = 3.14;
+
+    public void showTitle ( ) {
+        System.out.println("Welcome");
+    }
+    public int CalculateScore (int val) {
+
+       return val * val;
+    }
+    public double addNumbers(){
+        double PI = 3.0;
+        return PI;
+    }
+}
---
+++ JAVA/src/Week2/Exercises/Factorial.java
@@ -0,0 +1,4 @@
+package Week2.Exercises;
+
+public class Factorial {
+}
