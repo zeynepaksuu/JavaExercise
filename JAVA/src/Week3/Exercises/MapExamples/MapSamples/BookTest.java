8dacd5d80bda351aaf53d6141d6dbd71d18f6438
Zeynep Aksu <zeynepgspor@gmail.com>
2025-08-05 00:12:52 +0300

    updated

--- JAVA/src/Week3/Exercises/MapExamples/MapSamples/BookTest.java
+++ JAVA/src/Week3/Exercises/MapExamples/MapSamples/BookTest.java
@@ -1,4 +1,21 @@
 package Week3.Exercises.MapExamples.MapSamples;
+import java.util.HashMap;
+import java.util.Map;
+
+//kitabın arkasındaki barkodu tarattıgımızda kitabın bilgilerini bize getirsin
 
 public class BookTest {
+    public static void main (String[] args) {
+
+        Map<String, Book> bookmap = new HashMap<>();
+        bookmap.put("ISBN123", new Book("Clean Code", "Robert C. Martin"));
+        bookmap.put("ISBN345", new Book("Ateşten Gömlek", "Halide Edip Adıvar"));
+
+        for (Map.Entry<String, Book>entry:bookmap.entrySet()){
+            System.out.println("Key: " + entry.getKey() + "value : " + entry.getValue());
+
+        }
+
+
+    }
 }
